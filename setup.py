f7a1b6233280f756df524e6b70455b8607d30c44
Bogdan Mazoure <6981734+bmazoure@users.noreply.github.com>
2022-02-18 16:31:00 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='dreamerv2_jax',
+   version='0.0.1',
+   description='Dreamerv2 implemented in Jax',
+   author='Bogdan Mazoure',
+   author_email='bogdan.mazoure@mail.mcgill.ca',
+   packages=['dreamerv2'],  #same as name
+)
